0e616167809acfb940900cdffdd3593cee0b596e
jasmine <yasmine.ben-fredj@etu.unice.fr>
2020-09-24 21:02:24 +0200

    #16 Affichage du classement des joueurs

--- 7wonders/gameserver/src/main/java/servergame/card/ScoreCalculator.java
+++ 7wonders/gameserver/src/main/java/servergame/card/ScoreCalculator.java
@@ -1,8 +1,12 @@
 package servergame.card;
 
 import commun.card.Card;
+import commun.wonders.Player;
 import commun.card.WonderBoard;
+import log.GameLogger;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ScoreCalculator {
@@ -30,10 +34,11 @@ public class ScoreCalculator {
      */
     public Player winner(ArrayList<PLayer> players)
     {
+
         int winnerScore = getScore(players.getWonderBoard.get(1));
         Player winner= players.get(1);
-        for (Player player : players
-        ) {
+        for (Player player : players)
+        {
             if(winnerScore < getScore(player.getWonderBoard())){
                 winnerScore = getScore(player.getWonderBoard());
                 winner = player.getWonderBoard();
@@ -42,6 +47,44 @@ public class ScoreCalculator {
 
         }
         return winner;
+
+    }
+
+    /**
+     *
+     * Cette methode permet de classer les joueurs
+     * @param players List de tous les joueurs
+     * @return Retourne le classement dans une map , les clefs sont le classement
+     *
+     */
+    public Map<Integer,Player> ranking(ArrayList<PLayer> players)
+    {
+        Map<Integer, Player> ranking = new HashMap<Integer, Player>();
+        for(int i =1 ; i+1 < players.size();i++){
+            Player player = winner(players);
+            ranking.put(i,player);
+            players.remove(player);
+        }
+
+        return ranking;
+
+
+    }
+
+    /**
+     * Cette methode permet d'afficher le classement des joueurs
+     * @param players List des joueurs
+     *
+     */
+    public void printRanking(ArrayList<Player> players)
+    {
+        Map<Integer, Player> ranking = ranking(players);
+        GameLogger.log("Classement des joueurs : );" );
+
+        for (int i=1; i< ranking.size(); i++ ) {
+            GameLogger.log(i + " : " + ranking.get(i).getName);
+        }
+
 
     }
 
