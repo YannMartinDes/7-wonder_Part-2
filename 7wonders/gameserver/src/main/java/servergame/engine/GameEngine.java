83de11a8492615cb0724904406ab6ec018a468c8
Idrash <56682531+Idrash@users.noreply.github.com>
2020-09-24 20:34:35 +0200

    Moteur de jeu mis en place (sauf score) #18
    le moteur de jeu est mis en place il manque plus que le score final

--- 7wonders/gameserver/src/main/java/servergame/engine/GameEngine.java
+++ 7wonders/gameserver/src/main/java/servergame/engine/GameEngine.java
@@ -1,32 +1,50 @@
 package servergame.engine;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import commun.wonderboard.WonderBoard;
 import commun.wonders.Player;
 import servergame.card.CardManager;
+import servergame.wonderboard.WonderBoardFactory;
 
+/**
+ * Moteur de jeu qui a pour role de gerer le deroulement d'une partie
+ * elle gere les tour, les age et la fin de partie.
+ * @author Yohann
+ *
+ */
 public class GameEngine {
 	
 	private int nbPlayer;
 	private List<Player> allPlayers;
 	private CardManager cardManager;
 	
-	public GameEngine(int nbPlayer,List<Player> allPlayers) {
-		this.setNbPlayer(nbPlayer);
+	public GameEngine(List<Player> allPlayers) {
+		this.setNbPlayer(allPlayers.size());
 		this.allPlayers = allPlayers;
 		this.cardManager = new CardManager();
 	}
 	
+	
+	/**
+	 * Permet de lancer une parti
+	 */
 	public void startGame() {
 		cardManager.createHands(1);
+		assignPlayersWonderBoard();
 		assignPlayersDeck();
 		round();
 	}
 	
+	
+	/**
+	 * le deroulement d'un tour de jeu
+	 */
 	public void round() {
 		
 		for(Player player : allPlayers) {
-			player.iaPlay();
+			player.controllerPlay();
 		}
 		
 		for(Player player : allPlayers) {
@@ -36,12 +54,28 @@ public class GameEngine {
 		//TODO score calcule + display result
 	}
 	
-
-	public void assignPlayersDeck(){
+	
+	/**
+	 * on assigne le deck au joueur pour le tour qui commence
+	 */
+	private void assignPlayersDeck(){
 		for(int i =0; i<nbPlayer; i++) {
 			allPlayers.get(i).setCurrentDeck(cardManager.getHand(i));
 		}
 	}
+	
+	
+	/**
+	 * On assigne une merveille au joueur pour la parti
+	 */
+	private void assignPlayersWonderBoard(){
+		ArrayList<WonderBoard> wonders = new WonderBoardFactory().chooseWonderBoard(nbPlayer);
+		
+		for(int i =0; i<nbPlayer; i++) {
+			allPlayers.get(i).setWondersBoard(wonders.get(i));
+		}
+	}
+	
 	public int getNbPlayer() {
 		return nbPlayer;
 	}
