c7bce41aeb2deeb68cb430b3b7f11ad6e15bc073
jasmine <yasmine.ben-fredj@etu.unice.fr>
2020-09-24 21:55:34 +0200

    #46 Test ScoreCalculator

---
+++ 7wonders/gameserver/src/test/java/severgame/ScoreCalculatorTest.java
@@ -0,0 +1,96 @@
+package severgame;
+
+import commun.card.WonderBoard;
+import org.junit.jupiter.api.Test;
+
+import commun.wonders.Player;
+import commun.card.Card;
+import commun.card.CardType;
+import commun.effect.Effect;
+import commun.effect.VictoryPointEffect;
+import servergame.card.ScoreCalculator;
+
+import java.util.ArrayList;
+import servergame.card.ScoreCalculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ScoreCalculatorTest {
+
+    ScoreCalculator scoreCalculator= new ScoreCalculator();
+
+    WonderBoard wonderBoard1 = new WonderBoard("Alexandria");
+    WonderBoard wonderBoard2 = new WonderBoard("Rhodos");
+    WonderBoard wonderBoard3 = new WonderBoard("Gizah");
+
+
+    /**
+     * Ce test permet de verifier que la methode getScore calcule le bon score
+     */
+    @Test
+    public void getScoreTest()
+    {
+
+        ArrayList<WonderBoard> wonderBoards = new ArrayList<WonderBoard>();
+        wonderBoard2.addCardToBuilding(new Card("CivilBuilding", CardType.CIVIL_BUILDING,new VictoryPointEffect(3),1));
+        wonderBoard2.addCardToBuilding(new Card("CivilBuilding", CardType.CIVIL_BUILDING,new VictoryPointEffect(1),1));
+
+        assertEquals(scoreCalculator.getScore(wonderBoard2),4);
+
+    }
+
+
+
+    /**
+     * Ce test nous permet de verifier si la carte se créer correctement.
+     */
+    @Test
+    public void WinnerTest()
+    {
+
+        ArrayList<Player> players = new ArrayList<Player>();
+
+        wonderBoard1.addCardToBuilding(new Card("CivilBuilding", CardType.CIVIL_BUILDING,new VictoryPointEffect(1),1));
+        wonderBoard2.addCardToBuilding(new Card("CivilBuilding", CardType.CIVIL_BUILDING,new VictoryPointEffect(3),1));
+        wonderBoard3.addCardToBuilding(new Card("CivilBuilding", CardType.CIVIL_BUILDING,new VictoryPointEffect(1),1));
+
+        Player player1 = new Player("Player1",wonderBoard1);
+        PLayer player2 = new player("Player2",wonderBoard2);
+        PLayer player3 = new player("Player3",wonderBoard3);
+
+        players.add(player1);
+        players.add(player2);
+        players.add(player3);
+
+        Player theWinner = scoreCalculator.winner(players);
+
+        assertEquals(theWinner.getName, "Player3");
+
+
+    }
+
+    /**
+     * Ce test permet de verifier que le classement des joueurs est le bon
+     */
+    @Test
+    public void rankingTest()
+    {
+        ArrayList<Player> players = new ArrayList<Player>();
+        Player player1 = new Player("Player1",wonderBoard1);
+        PLayer player2 = new player("Player2",wonderBoard2);
+        PLayer player3 = new player("Player3",wonderBoard3);
+        wonderBoard1.addCardToBuilding(new Card("CivilBuilding", CardType.CIVIL_BUILDING,new VictoryPointEffect(1),1));
+        wonderBoard2.addCardToBuilding(new Card("CivilBuilding", CardType.CIVIL_BUILDING,new VictoryPointEffect(3),1));
+        wonderBoard3.addCardToBuilding(new Card("CivilBuilding", CardType.CIVIL_BUILDING,new VictoryPointEffect(2),1));
+
+        assertEquals(scoreCalculator.ranking(players).get(1),player2);
+        assertEquals(scoreCalculator.ranking(players).get(2),player3);
+        assertEquals(scoreCalculator.ranking(players).get(3),player1);
+
+
+    }
+
+
+
+
+}
