4f76d43444ad197310e759a0e6cea37c824404a9
Yann Martin D'Escrienne <yann.martindes@orange.fr>
2020-09-26 23:16:55 +0200

    Mise à jour des tests CardFactory

--- 7wonders/gameserver/src/test/java/severgame/card/CardFactoryTest.java
+++ 7wonders/gameserver/src/test/java/severgame/card/CardFactoryTest.java
@@ -4,8 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import commun.card.CardType;
 import commun.card.Deck;
+import commun.effect.AddindChoiceMaterialEffect;
+import commun.effect.AddingMaterialEffet;
 import commun.effect.IEffect;
 import commun.effect.VictoryPointEffect;
+import commun.material.ChoiceMaterial;
+import commun.material.Material;
+import commun.material.MaterialType;
 import org.junit.jupiter.api.*;
 import servergame.card.CardFactory;
 
@@ -30,7 +35,21 @@ public class CardFactoryTest
                 "AUTEL",
                 "AUTEL",
                 "THÉÂTRE",
-                "THÉÂTRE"};
+                "THÉÂTRE",
+                "CHANTIER",
+                "CHANTIER",
+                "CAVITÉ",
+                "CAVITÉ",
+                "BASSIN ARGILEUX",
+                "BASSIN ARGILEUX",
+                "FILON",
+                "FILON",
+                "FRICHE",
+                "EXCAVATION",
+                "FOSSE ARGILEUSE",
+                "EXPLOITATION FORESTIÉRE",
+                "GISEMENT",
+                "MINE"};
 
         Deck deckGot = this.cardFactory.AgeOneCards();
 
@@ -51,55 +70,71 @@ public class CardFactoryTest
                 new VictoryPointEffect(2),
                 new VictoryPointEffect(2),
                 new VictoryPointEffect(2),
-                new VictoryPointEffect(2)};
+                new VictoryPointEffect(2),
+
+                new AddingMaterialEffet(new Material(MaterialType.WOOD,1 )),
+                new AddingMaterialEffet(new Material(MaterialType.WOOD,1 )),
+                new AddingMaterialEffet(new Material(MaterialType.STONE,1 )),
+                new AddingMaterialEffet(new Material(MaterialType.STONE,1 )),
+                new AddingMaterialEffet(new Material(MaterialType.CLAY,1 )),
+                new AddingMaterialEffet(new Material(MaterialType.CLAY,1 )),
+                new AddingMaterialEffet(new Material(MaterialType.ORES,1 )),
+                new AddingMaterialEffet(new Material(MaterialType.ORES,1 )),
+
+                new AddindChoiceMaterialEffect(new ChoiceMaterial( new Material(MaterialType.WOOD,1) , new Material(MaterialType.CLAY,1))),
+                new AddindChoiceMaterialEffect(new ChoiceMaterial( new Material(MaterialType.STONE,1) , new Material(MaterialType.CLAY,1))),
+                new AddindChoiceMaterialEffect(new ChoiceMaterial( new Material(MaterialType.CLAY,1) , new Material(MaterialType.ORES,1))),
+                new AddindChoiceMaterialEffect(new ChoiceMaterial( new Material(MaterialType.STONE,1) , new Material(MaterialType.WOOD,1))),
+                new AddindChoiceMaterialEffect(new ChoiceMaterial( new Material(MaterialType.WOOD,1) , new Material(MaterialType.ORES,1))),
+                new AddindChoiceMaterialEffect(new ChoiceMaterial( new Material(MaterialType.STONE,1) , new Material(MaterialType.ORES,1))),
+
+        };
 
         Deck deckGot = this.cardFactory.AgeOneCards();
 
         for (int i = 0; i < deckGot.getLength(); i++)
         {
             assertEquals(deckGot.getCard(i).getCardEffect().getScore(), expected[i].getScore());
+
+            if(deckGot.getCard(i).getCardEffect().getMaterial() != null){
+                assertEquals(deckGot.getCard(i).getCardEffect().getMaterial().getNumber(), expected[i].getMaterial().getNumber());
+                assertEquals(deckGot.getCard(i).getCardEffect().getMaterial().getType(), expected[i].getMaterial().getType());
+            }
+
+            if(deckGot.getCard(i).getCardEffect().getChoiceMaterial() != null){
+                assertEquals(deckGot.getCard(i).getCardEffect().getChoiceMaterial().getMaterial1().getType(), expected[i].getChoiceMaterial().getMaterial1().getType());
+                assertEquals(deckGot.getCard(i).getCardEffect().getChoiceMaterial().getMaterial1().getNumber(), expected[i].getChoiceMaterial().getMaterial1().getNumber());
+
+                assertEquals(deckGot.getCard(i).getCardEffect().getChoiceMaterial().getMaterial2().getType(), expected[i].getChoiceMaterial().getMaterial2().getType());
+                assertEquals(deckGot.getCard(i).getCardEffect().getChoiceMaterial().getMaterial2().getNumber(), expected[i].getChoiceMaterial().getMaterial2().getNumber());
+            }
+
+
         }
     }
 
     @Test
     public void testAgeOneCardsType ()
     {
-        CardType[] expected = new CardType [] {
-                CardType.CIVIL_BUILDING,
-                CardType.CIVIL_BUILDING,
-                CardType.CIVIL_BUILDING,
-                CardType.CIVIL_BUILDING,
-                CardType.CIVIL_BUILDING,
-                CardType.CIVIL_BUILDING,
-                CardType.CIVIL_BUILDING,
-                CardType.CIVIL_BUILDING};
-
         Deck deckGot = this.cardFactory.AgeOneCards();
 
         for (int i = 0; i < deckGot.getLength(); i++)
         {
-            assertEquals(deckGot.getCard(i).getType(), expected[i]);
+            if(i<8)
+                assertEquals(deckGot.getCard(i).getType(), CardType.CIVIL_BUILDING);
+            else if(i<22)
+                assertEquals(deckGot.getCard(i).getType(), CardType.RAW_MATERIALS);
         }
     }
 
     @Test
     public void testAgeOneCardsAge ()
     {
-        Integer[] expected = new Integer [] {
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1};
-
         Deck deckGot = this.cardFactory.AgeOneCards();
 
         for (int i = 0; i < deckGot.getLength(); i++)
         {
-            assertEquals(deckGot.getCard(i).getAge(), expected[i]);
+            assertEquals(deckGot.getCard(i).getAge(), 1);//Age 1
         }
     }
 }
