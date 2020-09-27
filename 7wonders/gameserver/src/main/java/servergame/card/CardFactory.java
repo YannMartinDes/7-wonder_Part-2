1d835efe9c0482f7c114be962d8d2ebe4d8d183f
Sébastien Marro <marrosebastien@protonmail.com>
2020-09-27 16:10:09 +0200

    #90  Ajout des cartes Produits manufacturés

--- 7wonders/gameserver/src/main/java/servergame/card/CardFactory.java
+++ 7wonders/gameserver/src/main/java/servergame/card/CardFactory.java
@@ -48,7 +48,21 @@ public class CardFactory {
         deck1.addCard(new Card("EXPLOITATION FORESTIÉRE", CardType.RAW_MATERIALS, new AddindChoiceMaterialEffect(new ChoiceMaterial( new Material(MaterialType.STONE,1) , new Material(MaterialType.WOOD,1))),1,new CoinCost(1))); //3+
         deck1.addCard(new Card("GISEMENT", CardType.RAW_MATERIALS, new AddindChoiceMaterialEffect(new ChoiceMaterial( new Material(MaterialType.WOOD,1) , new Material(MaterialType.ORES,1))),1,new CoinCost(1))); //5+
         deck1.addCard(new Card("MINE", CardType.RAW_MATERIALS, new AddindChoiceMaterialEffect(new ChoiceMaterial( new Material(MaterialType.STONE,1) , new Material(MaterialType.ORES,1))),1,new CoinCost(1))); //6+
-        
+
+        //Cartes Grises (Produits manufacturés)
+        deck1.addCard(new Card("VERRERIE", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.GLASS, 1)), 1,null));//3+
+        deck1.addCard(new Card("VERRERIE", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.GLASS, 1)), 1,null));//3+
+        deck1.addCard(new Card("VERRERIE", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.GLASS, 1)), 1,null));//5+
+        deck1.addCard(new Card("VERRERIE", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.GLASS, 1)), 1,null));//6+
+        deck1.addCard(new Card("MÉTIER À TISSER", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.FABRIC,1)),1,null));//3+
+        deck1.addCard(new Card("MÉTIER À TISSER", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.FABRIC,1)),1,null));//3+
+        deck1.addCard(new Card("MÉTIER À TISSER", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.FABRIC,1)),1,null));//5+
+        deck1.addCard(new Card("MÉTIER À TISSER", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.FABRIC,1)),1,null));//6+
+        deck1.addCard(new Card("PRESSE", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.PAPYRUS, 1)), 1,null));//3+
+        deck1.addCard(new Card("PRESSE", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.PAPYRUS, 1)), 1,null));//3+
+        deck1.addCard(new Card("PRESSE", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.PAPYRUS, 1)), 1,null));//5+
+        deck1.addCard(new Card("PRESSE", CardType.RAW_MATERIALS, new AddingMaterialEffet(new Material(MaterialType.PAPYRUS, 1)), 1,null));//6+
+
         return deck1;
     }
 }
