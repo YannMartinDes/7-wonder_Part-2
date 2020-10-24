32c20aaf079cb7a6b6c640dc05b7c7f2991cd6a4
theoricien <letheoricien@hotmail.com>
2020-10-24 17:53:29 +0200

    (#282) Test JsonUtils

---
+++ 7wonders/commun/src/test/java/commun/communication/JsonUtilsTest.java
@@ -0,0 +1,40 @@
+package commun.communication;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonUtilsTest
+{
+    private JsonUtils jsonUtils;
+    private ArrayList<Integer> arrayList;
+
+    @BeforeEach
+    public void init ()
+    {
+        this.jsonUtils = new JsonUtils();
+        this.arrayList = new ArrayList<>();
+        this.arrayList.add(0);
+        this.arrayList.add(1);
+        this.arrayList.add(2);
+        this.arrayList.add(3);
+    }
+
+    @Test
+    public void testSerialize ()
+            throws IOException
+    {
+        String expected = "{\"object\":[0,1,2,3],\"type\":\"java.util.ArrayList\"}";
+        assertEquals(expected, this.jsonUtils.serialize(this.arrayList));
+    }
+
+    @Test
+    public void testDeserialize ()
+            throws IOException, ClassNotFoundException
+    {
+        assertEquals(this.arrayList.toString(), this.jsonUtils.deserialize(this.jsonUtils.serialize(this.arrayList), ArrayList.class).toString());
+    }
+}
