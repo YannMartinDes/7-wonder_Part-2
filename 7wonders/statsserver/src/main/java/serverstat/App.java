29229144cf3f98068b0129a1a4a7c0d26e9491e5
TOGNETTI Yohann <56682531+Idrash@users.noreply.github.com>
2021-02-15 19:47:11 +0100

    #16 lancement du serveur de stat par Spring
    Le serveur de stat est maintenant lancer par Spring

--- 7wonders/statsserver/src/main/java/serverstat/App.java
+++ 7wonders/statsserver/src/main/java/serverstat/App.java
@@ -1,15 +1,23 @@
 package serverstat;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import serverstat.server.Server;
 
 import java.net.UnknownHostException;
+import java.util.Collections;
 
+
+@SpringBootApplication
 public class App {
 
 	public static void main(String[] args) throws UnknownHostException {
-
-		Server server = new Server();
-		server.startServer();
+		SpringApplication app = new SpringApplication(App.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "1334"));
+		app.run();
+//		Server server = new Server();
+//		server.startServer();
 
 	}
 }
