f6d7a5f77bfa39ece79d904afd53fa1ffdd2ccf2
theoricien <letheoricien@hotmail.com>
2021-03-16 10:43:00 +0100

    (#48) RestTemplate pour le GameServer

---
+++ 7wonders/gameserver/src/main/java/servergame/player/rest/RequestToPlayerRestTemplate.java
@@ -0,0 +1,83 @@
+package servergame.player.rest;
+
+import commun.action.AbstractAction;
+import commun.card.Deck;
+import commun.communication.CommunicationMessages;
+import commun.effect.ScientificType;
+import commun.request.ID;
+import commun.request.RequestToPlayer;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/** Explicit class */
+public class RequestToPlayerRestTemplate implements RequestToPlayer
+{
+    private RestTemplate restTemplate;
+    private HttpHeaders headers;
+    private ID id;
+
+    public RequestToPlayerRestTemplate (ID id)
+    {
+        this.restTemplate = new RestTemplate();
+        this.id = id;
+    }
+
+    @Override
+    public AbstractAction chooseAction (Deck deck)
+    {
+        ResponseEntity<AbstractAction> response;
+        HttpEntity<Deck> httpEntity;
+
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        httpEntity = new HttpEntity<>(deck, headers);
+
+        response = restTemplate.postForEntity(this.id.getUri() + "/" + CommunicationMessages.CHOOSEACTION, httpEntity, AbstractAction.class);
+
+        return response.getBody();
+    }
+
+    @Override
+    public Integer[] choosePurchasePossibility (List<Integer[]> purchaseChoice)
+    {
+        ResponseEntity<Integer[] > response;
+        HttpEntity<List<Integer[]>> httpEntity;
+
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        httpEntity = new HttpEntity<>(purchaseChoice, headers);
+
+        response = restTemplate.postForEntity(this.id.getUri() + "/" + CommunicationMessages.CHOOSEPURCHASE, httpEntity, Integer[].class);
+
+        return response.getBody();
+    }
+
+    @Override
+    public ScientificType useScientificsGuildEffect ()
+    {
+        ResponseEntity<ScientificType> response;
+
+        response = restTemplate.getForEntity(this.id.getUri() + "/" + CommunicationMessages.CHOOSESCIENTIFICS, ScientificType.class);
+        return response.getBody();
+    }
+
+    @Override
+    public int chooseCard (Deck deck)
+    {
+        ResponseEntity<Integer> response;
+        HttpEntity<Deck> httpEntity;
+
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        httpEntity = new HttpEntity<>(deck, headers);
+
+        response = restTemplate.postForEntity(this.id.getUri() + "/" + CommunicationMessages.CHOOSECARD, httpEntity, Integer.class);
+
+        return response.getBody();
+    }
+}
