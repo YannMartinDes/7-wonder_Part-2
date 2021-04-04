d0ce6bda53e43c232baa83a95eeec757dea37f5b
Mentra20 <yann.martindes@orange.fr>
2021-04-04 17:36:16 +0200

    #73 PlayerBoardController

--- 7wonders/client/src/main/java/client/App.java
+++ 7wonders/client/src/main/java/client/App.java
@@ -1,31 +1,9 @@
 package client;
 
-import client.AI.AI;
-import client.AI.FirstAI;
-import client.AI.RandomAI;
-import client.AI.SecondAI;
-import client.playerRestTemplate.InscriptionRestTemplate;
-import client.playerRestTemplate.PlayerRestTemplate;
-import client.utils.CommunicationUtils;
-import commun.request.ID;
-import commun.request.PlayerRequestGame;
-import log.Logger;
-import org.apache.juli.logging.Log;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.HashMap;
-import java.util.Random;
 
 @SpringBootApplication
 public class App {
