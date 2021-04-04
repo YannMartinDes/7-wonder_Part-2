2561a88768add24a0392696216e480de5563f136
theoricien <letheoricien@hotmail.com>
2021-04-04 19:17:15 +0200

    :)

--- 7wonders/client/src/main/java/client/utils/CommunicationUtils.java
+++ 7wonders/client/src/main/java/client/utils/CommunicationUtils.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 /**
