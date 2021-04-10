857318686c97b4d4e7ba9f9369afabd23278c44f
Mentra20 <yann.martindes@orange.fr>
2021-04-10 11:45:15 +0200

    #91 ChooseAction Test IT

--- 7wonders/gameserver/src/test/java/servergame/integration/RequestToPlayerIT.java
+++ 7wonders/gameserver/src/test/java/servergame/integration/RequestToPlayerIT.java
@@ -1,12 +1,22 @@
 package servergame.integration;
 
+import commun.action.AbstractAction;
+import commun.card.Card;
+import commun.card.CardType;
 import commun.card.Deck;
 import commun.communication.CommunicationMessages;
 import commun.communication.StatObject;
+import commun.cost.CoinCost;
+import commun.effect.ChoiceMaterialEffect;
+import commun.effect.CoinEffect;
+import commun.material.ChoiceMaterial;
+import commun.material.Material;
+import commun.material.MaterialType;
 import commun.player.Player;
 import commun.request.ID;
 import commun.request.RequestToPlayer;
 import commun.wonderboard.WonderBoard;
+import io.cucumber.java8.De;
 import log.Logger;
 import org.junit.After;
 import org.junit.Before;
@@ -33,8 +43,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
 
 @SpringBootTest(classes = {App.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ExtendWith(SpringExtension.class)
@@ -79,7 +91,7 @@ public class RequestToPlayerIT {
         playerBoardController.setPlayers(new ArrayList<>());
         for(int i = 0; i<nbPlayer;i++){
             Player player = new Player(allPlayer.get(i).getName());
-            player.setWonderBoard(new WonderBoard());
+            player.setWonderBoard(new WonderBoard("wonderBoard", new ChoiceMaterialEffect(new ChoiceMaterial(new Material(MaterialType.STONE,1)))));
             player.setCurrentDeck(new Deck());
 
             playerBoardController.getPlayers().add(player);
@@ -97,7 +109,7 @@ public class RequestToPlayerIT {
             //on essayer d'assigner le nombre total de joueur a chaque joueur
             ResponseEntity response = inscriptionPlayer.sendNbPlayers(allPlayer.get(i));
             //On appelle bien un post
-            Mockito.verify(restTemplate, Mockito.times(1)).postForEntity(eq(allPlayer.get(i).getUri() + "/nplayers"), any(), any());
+            Mockito.verify(restTemplate, times(1)).postForEntity(eq(allPlayer.get(i).getUri() + "/nplayers"), any(), any());
             //on dois recevoir un OK pour dire que c'est ok
             assertEquals(response.getStatusCode(), HttpStatus.OK);
         }
@@ -113,9 +125,48 @@ public class RequestToPlayerIT {
             //on essayer d'assigner le nombre total de joueur a chaque joueur
             ResponseEntity response = inscriptionPlayer.sendPlayerPosition(allPlayer.get(i),i);
             //On appelle bien un post
-            Mockito.verify(restTemplate, Mockito.times(1)).postForEntity(eq(allPlayer.get(i).getUri() + "/id"), any(), any());
+            Mockito.verify(restTemplate, times(1)).postForEntity(eq(allPlayer.get(i).getUri() + "/id"), any(), any());
             //on dois recevoir un OK pour dire que c'est ok
             assertEquals(response.getStatusCode(), HttpStatus.OK);
+        }
+    }
+
+    @Test
+    public void chooseActionTest(){
+        int i = 0;
+        for(RequestToPlayer rtp : requestToPlayer){
+            //Cast
+            RequestToPlayerRestTemplate rtprt = (RequestToPlayerRestTemplate) rtp;
+
+            restTemplate = Mockito.spy(new RestTemplate());
+            ReflectionTestUtils.setField(rtprt, "restTemplate", restTemplate);
+
+            //Creation d'un deck
+            Deck deck = new Deck();
+            deck.add(new Card("t", CardType.MANUFACTURED_PRODUCTS,new CoinEffect(5),1,new CoinCost(1),""));
+            deck.add(new Card("t", CardType.MANUFACTURED_PRODUCTS,new CoinEffect(5),1,new CoinCost(1),""));
+            deck.add(new Card("t", CardType.MANUFACTURED_PRODUCTS,new CoinEffect(5),1,new CoinCost(1),""));
+            deck.add(new Card("t", CardType.MANUFACTURED_PRODUCTS,new CoinEffect(5),1,new CoinCost(1),""));
+
+            //Info nécéssaire pour /ID
+            inscriptionPlayer.sendNbPlayers(rtprt.getID());
+            inscriptionPlayer.sendPlayerPosition(rtprt.getID(),i);
+
+            //Appel de la méthode
+            AbstractAction action = rtprt.chooseAction(deck);
+
+            //On recois bien une action valide
+            assertTrue(action != null);
+            assertTrue(action.getIndexOfCard() >= 0 && action.getIndexOfCard() <4);
+
+            //On appelle bien un post
+            Mockito.verify(restTemplate, times(1)).postForEntity(eq(rtprt.getID().getUri() + CommunicationMessages.CHOOSEACTION), any(), any());
+
+            //Seul ces IA cherchent à connaitre leur plateau
+            if(rtprt.getID().getStrategy() == "SecondAI" || rtprt.getID().getStrategy() == "FirstAI"){
+                Mockito.verify(playerBoardController,times(1)).loadBoard(anyString());
+            }
+            i++;
         }
     }
 
