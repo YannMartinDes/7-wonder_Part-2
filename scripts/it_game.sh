34f0c07b18d80f22c2ae32d10b6a18c4c8eaa642
Idrash <tognetti.yohann@outlook.fr>
2021-04-11 21:55:52 +0200

    Ajout commentaire script IT_game

--- scripts/it_game.sh
+++ scripts/it_game.sh
@@ -22,7 +22,6 @@ game ()
     NB_CLIENTS='2'
     # CLIENTS=client1 client2 ... clientN
     CLIENTS=$(python3 -c "print(\" \".join([\"$DOCKER_CONTAINER{}\".format(i) for i in range(1, $NB_CLIENTS + 1)]), end=\"\")")
-    alias mvn='mvn $@ | sed "s/Tests run: 0, Failures: 0, Errors: 0, Skipped: 0/\x1B\[1;32mTests run: 1, Failures: 0, Errors: 0, Skipped: 0\x1B[0m/g"'
     # Flags IT
     IT_GAME_URI='-DgameServer.uri=http://localhost:1336'
     IT_GAME_IP='-DIP=172.22.0.1'
@@ -58,6 +57,7 @@ game ()
         debug docker run -e GAME_IP=$GAME_IP -d --name $DOCKER_CONTAINER$i $PREFIX_DOCKER_TAG:$PACKAGE_CLIENT
     done
 
+    #on lance notre ia qui vas pouvoir faire les test au cours de la partie
     debug ${INTEGRATION_TEST_MVN}canCheckAllPlayerBoard
 }
 
